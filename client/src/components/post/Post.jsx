c71603becdfff307afbd703d1135574ae42e5a3c
patto220048 <220048xn@yse-c.net>
2023-02-10 08:52:29 +0900

    .

--- client/src/components/post/Post.jsx
+++ client/src/components/post/Post.jsx
@@ -4,7 +4,44 @@ function Post() {
     return ( 
         <div className="post-container">
             <div className="post-wapper">
-                Post
+                <div className="post-items">
+                    <div className="user-info">
+                        <img src="" alt="" />
+                        <div className="name-user">
+                            <span>PHAT</span>
+                            <div className="time">6 hour ago</div>
+                        </div>
+
+                    </div>
+                    <div className="post-img">
+                        <img src="" alt="" />
+                    </div>
+                    <div className="post-info">
+                        <div className="like-number">
+
+                        </div>
+                        <div className="comment-number">
+
+                        </div>
+                        <div className="share-number">
+
+                        </div>
+
+                    </div>
+                    <div className="post-action">
+                        <div className="like">
+
+                        </div>
+                        <div className="comments">
+                        
+                        </div>
+                        <div className="share">
+
+                        </div>
+                    </div>
+
+
+                </div>
             </div>
         </div>
      );
