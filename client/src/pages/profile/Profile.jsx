23de9512f520201489f1a2b029498c284a057b0c
patto220048 <220048xn@yse-c.net>
2023-02-14 01:26:24 +0900

    update profile left

--- client/src/pages/profile/Profile.jsx
+++ client/src/pages/profile/Profile.jsx
@@ -2,12 +2,14 @@ import { useState } from 'react';
 import Post from '../../components/post/Post';
 import Navbar from '../../layout/navbar/Navbar';
 import Sidebar from '../../layout/sidebar/Sidebar';
+import Contents from '../../layout/contents/Contents';
+
 import AddIcon from '@mui/icons-material/Add';
 import './profile.scss'
 
 
 function Profile({openSearch,setOpenSearch}) {
-
+    const [openEditDesc,setOpenEditDesc] = useState(false)
 
     return ( 
         <>
@@ -21,10 +23,10 @@ function Profile({openSearch,setOpenSearch}) {
                             <img className='background-img' src="https://images.unsplash.com/photo-1676113462035-e4d56ce608d1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzNnx8fGVufDB8fHx8&auto=format&fit=crop&w=2000&q=60" alt="" />
                             <button><AddIcon fontSize='large'  className='add-icon'/></button>
                         </div>
-                        <div className="btn-fl">
+                        {/* <div className="btn-fl">
                             <button className='follow1'>Follow</button>
                             <button className='follow2'>Follow</button>  
-                        </div>
+                        </div> */}
                         <div className="avatar">
                             <img className='avatar-img' src="https://images.unsplash.com/photo-1675621929929-a8a74c6b795e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzMXx8fGVufDB8fHx8&auto=format&fit=crop&w=2000&q=60" alt="" />
                             <button className='avatar-add-icon'>
@@ -32,20 +34,72 @@ function Profile({openSearch,setOpenSearch}) {
                             </button>
                            
                         </div>
+                        <h1 className="name-user">DINH HUU PHAT</h1>
                         <div className="profile-mid">
-                            <p className='post-count'><span>MY POST </span>2</p>
-                            <p className='follow-count'><span>FOLLOWER </span>1000</p>
-                            <p className='friend-count'><span>FRIENDS </span>245</p>
+                            <p className='post-count'><span>2 </span>following</p>
+                            <p className='follow-count'><span>1000 </span>follower</p>
+                            <p className='friend-count'><span>1 </span>post</p>
 
                         </div>
                     </div>
                     <div className="profile-bottom">
-                          <Post/>
-                          <Post/>
-                          <Post/>
-                          <Post/>
-                          <Post/>
-                          <Post/>
+                        <div className="left">
+                            <h1 className='introduce'>Introduce</h1>
+                            <div className="introduce-item">
+                                <div className="desc">
+                                    <p className='desc-text' >hello guys</p>
+                                    { openEditDesc ? 
+                                    <div className="edit">
+                                        <textarea name="" id="" cols="50" rows="4"></textarea>
+                                        <div className="edit-desc">
+                                            <button className='cancel-btn' onClick={()=>setOpenEditDesc(!openEditDesc)}> Cancel</button>
+                                            <button className ='update-btn'> Update</button>
+                                        </div>
+
+                                    </div>
+                                    :
+                                    <button className ='edit-btn' onClick={()=>setOpenEditDesc(!openEditDesc)}> Edit</button>
+                                    }                                   
+                                </div>              
+            
+                            </div>
+                            <div className="info">
+                                <h1 className='info-detail'>Details</h1>
+
+                                <div className="info-item">
+                                    <span>1</span>
+                                    <span>1</span>
+                                    <span>1</span>
+                                    <span>1</span>
+                                    <button className="info-btn">
+                                    
+                                    </button>
+                                </div>
+                                <div className="edit-info">
+
+                                    <input type="text" />
+                                    <input type="text" />
+                                    <input type="text" />
+                                    <input type="text" />
+                                    <div className="btn-info-edit">
+                                        <button className="cancel-btn">
+
+                                        </button>
+                                        <button className="update-btn">
+                                            
+                                        </button>
+
+                                    </div>
+                                </div>
+                                
+                            </div>
+                            
+                        </div>
+                        <div className="right">
+                           <Contents/>
+                        </div>
+
+                      
                     </div>
 
                   
